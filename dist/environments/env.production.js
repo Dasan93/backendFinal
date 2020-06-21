d17eae4ddc6787e16a0828a635e1d45ce4ba2116
salvador_sanchez <daniel_sanche2@hotmail.com>
2020-06-21 12:49:46 -0500

    first commit

---
+++ dist/environments/env.production.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    API: {
+        NAME: "API de autenticación",
+        PORT: 3000,
+        ENVIRONMENT: 'Development'
+    },
+    MONGODB: {
+        PORT: 27017,
+        HOST: '192.168.1.151',
+        USER_NAME: 'dba-Auth',
+        USER_PASSWORD: 'admin1234',
+        DEFAULT_DATABASE: 'dbauth'
+    }
+};
---
+++ environments/env.production.ts
@@ -0,0 +1,14 @@
+export default {
+    API: {
+        NAME: "API de autenticación",
+        PORT: 3000,
+        ENVIRONMENT: 'Development'
+    },
+    MONGODB: {
+        PORT: 27017,
+        HOST: '192.168.1.151',
+        USER_NAME: 'dba-Auth',
+        USER_PASSWORD: 'admin1234',
+        DEFAULT_DATABASE: 'dbauth'
+    }
+}
